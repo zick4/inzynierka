8976c5f44bd5f4659d27f1588da622b83eec5dc4
Pawel Grzeszczak <pgrzeszczak.mail@gmail.com>
2011-07-09 02:40:20 +0200

    Wprowadzono bibliotek Doctrine 1.2

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -26,13 +26,40 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         Zend_Registry::set('config_forms', new Zend_Config_Ini(APPLICATION_PATH . "/configs/forms.ini"));
     }
 
-//    protected function _initDb()
-//    {
-////$this->bootstrap('db');
-//        $dbAdapter = $this->getResource('db');
-////Debug::dump("1");
-//
-//    }
+    protected function _initDoctrine()
+    {
+        $this->getApplication()->
+                getAutoloader()->
+                pushAutoloader(array('Doctrine_Core', 'autoload'));
+
+        $config = $this->getOption('doctrine');
+        $manager = Doctrine_Manager::getInstance();
+        foreach ($config as $key => $value)
+        {
+            if ($key != 'connection')
+                $manager->setAttribute($key, $value);
+        }
+
+//        $manager->setAttribute(Doctrine_Core::ATTR_MODEL_CLASS_PREFIX, 'Model_');
+//        $manager->setAttribute(Doctrine_Core::ATTR_MODEL_LOADING, Doctrine_Core::MODEL_LOADING_PEAR);
+//        $manager->setAttribute(Doctrine_Core::ATTR_VALIDATE, Doctrine_Core::VALIDATE_ALL);
+//        $manager->setAttribute(Doctrine_Core::ATTR_USE_DQL_CALLBACKS, true);
+//        $manager->setAttribute(Doctrine_Core::ATTR_AUTO_FREE_QUERY_OBJECTS, true);
+//        $manager->setAttribute(Doctrine_Core::ATTR_AUTO_ACCESSOR_OVERRIDE, true);
+//        $manager->setAttribute(Doctrine_Core::ATTR_AUTOLOAD_TABLE_CLASSES, true);
+
+        if (isset($config['cache']) && $config['cache'] == true)
+        {
+            $cacheDriver = new Doctrine_Cache_Apc();
+            $manager->setAttribute(Doctrine_Core::ATTR_QUERY_CACHE, $cacheDriver);
+        }
+
+        $connection = $manager->openConnection($config['connection'], 'doctrine');
+        $connection->setAttribute(Doctrine_Core::ATTR_USE_NATIVE_ENUM, true);
+        $connection->setCharset('utf8');
+
+        return $connection;
+    }
 
 }
 
