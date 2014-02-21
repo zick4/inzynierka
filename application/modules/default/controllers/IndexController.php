52056e9a2f2158f4b93e50ececb6d184128a19ed
Paweł Grzeszczak <pawel.grzeszczak@nextwebventures.com>
2014-02-21 13:40:24 +0100

    Pozbycie się zbędnej metody

--- application/modules/default/controllers/IndexController.php
+++ application/modules/default/controllers/IndexController.php
@@ -3,11 +3,6 @@
 class IndexController extends App_Controller
 {
 
-    public function init()
-    {
-        /* Initialize action controller here */
-    }
-
     public function indexAction()
     {
         if (Zend_Auth::getInstance()->hasIdentity()) {
